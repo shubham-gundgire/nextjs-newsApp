5ef6a4e0bb69bbbb2c97252a960f7a563169f7ec
shubham <shubhamgundagire@gmail.com>
2021-11-21 13:46:23 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css';
+import { Toolbar } from '../components/toolbar';
+
+export default function Home() {
+  return (
+    <div className="page-container">
+      <Toolbar />
+
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+        <h5>this project created by following tutorial. link to tutorial <a  style={{color:"skyblue",textDecoration:"underline"}} href="https://www.youtube.com/watch?v=xtItzwYG6oQ">tutorial</a> </h5>
+      
+      </div>
+    </div>
+  );
+}
